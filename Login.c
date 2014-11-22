eecbf67796793f5e855ede99edafd466c598258d
Zi Yu ZHAO <zzhao8@teaching.CS.McGill.CA>
2014-11-21 20:40:07 -0500

    Incorporated Post

--- Login.c
+++ Login.c
@@ -39,9 +39,9 @@ int Verify(char* username, char* password){
 void verifySuccess(int result){
 //Method takes in rsult of verification and act appropriately
 	if(result==0){
-		printf("The username and password matched!\n");
+		printf("<p>The username and password matched!\n");
 	}
-	else printf("Your identification did not match, please try again\n");
+	else printf("<p>Your identification did not match, please try again\n");
 }
 int main(void){
 	//1-receive 2 strings from page
@@ -57,19 +57,30 @@ int main(void){
 	verifySuccess(result);
 	return 0;
 */
-	char* fn;
-	char* ln;
-	char*data;
-	fn=malloc(1000);
-	ln=malloc(1000);
+	char* un;
+	char* pw;
+	char* input;
+	//int inLength=atoi(getenv("CONTENT_LENGTH"));//get from shell length of input
+	un=malloc(1000);
+	pw=malloc(1000);
+	input=malloc(1000);
 	printf("%s%c%c\n","Content-Type:text/html;charset=iso-8859-1",13,10);
 	printf("<title>Login Details</title>\n");
-	printf("<h3>Login Details</h3>\n");
-	data=getenv("QUERY_STRING");
-	printf("<p> Info is: \n <br> %s",data);
+	fgets(input,200,stdin);
+	printf("<p> Info is \n <br> %s\n",input);
+	//I now have the input as a string separated by &s and =s
+	un=strtok(input,"=");
+	un=strtok(NULL,"=");
+	pw=strtok(NULL,"=");
+	un=strtok(un,"&");
+	//Now username & pw has been read.
+	int result=Verify(un,pw);	
+	if(result==0){//Match
 
+	}
+	else{//Did not match
 
-
-
+	}
+	return 0;
 }
 
